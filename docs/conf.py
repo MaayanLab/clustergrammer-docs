c9ea6802062b0f13f5b2ebb565e396aa495fe71e
cornhundred <nickfloresfernandez@gmail.com>
2017-02-16 20:25:27 -0500

    added print statement

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,11 @@ sphinx.environment.BuildEnvironment.warn_node = _warn_node
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+# sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../clustergrammer/__init__.py'))
+
+print('********** checking sys.path')
+print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
