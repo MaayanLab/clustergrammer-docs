270e65aa1f0d36e6ccb21e7b6471da2e53d9f093
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-10 18:41:25 -0500

    using latest version of .py

--- clustergrammer_py/__init__.py
+++ clustergrammer_py/__init__.py
@@ -1,6 +1,6 @@
 class Network(object):
   '''
-  version 1.5.0
+  version 1.5.1
 
   Clustergrammer.py takes a matrix as input (either from a file of a Pandas DataFrame), normalizes/filters, hierarchically clusters, and produces the :ref:`visualization_json` for :ref:`clustergrammer_js`.
 
